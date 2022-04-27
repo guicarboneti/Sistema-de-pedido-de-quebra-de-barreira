3b655df5b741f35b6af9f2af8f26fd9def446be3
leticiafontanelli <leticiafontanelli.ll@gmail.com>
2022-04-27 00:37:24 -0300

    Add evento de botao e guarda respostas checkbox

--- Interface.java
+++ Interface.java
@@ -14,6 +14,7 @@ import java.awt.Color;
 import java.awt.SystemColor;
 import javax.swing.SwingConstants;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.JOptionPane;
 
 import java.awt.Font;
 import java.awt.FlowLayout;
@@ -23,14 +24,41 @@ import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 
+import java.util.ArrayList;
+
 public class Interface {
 
     private JFrame frame;
-    private JTextField textField;
-    private JTextField textField_1;
-    private JTextField textField_2;
+    private JTextField textNome;
+    private JTextField textGrr;
+    private JTextField textCaminho;
+    private JCheckBox chckbxCi;
+    private JCheckBox chckbxCi_1;
+    private JCheckBox chckbxCi_2;
+    private JCheckBox chckbxCi_3;
+    private JCheckBox chckbxCi_4;
+    private JCheckBox chckbxCi_5;
+    private JCheckBox chckbxCi_6;
+    private JCheckBox chckbxCi_7;
+    private JCheckBox chckbxCi_8;
+    private JCheckBox chckbxCi_9;
+    private JCheckBox chckbxCi_10;
+    private JCheckBox chckbxCi_11;
+    private JCheckBox chckbxCi_12;
+    private JCheckBox chckbxCi_13;
+    private JCheckBox chckbxCi_14;
+    private JCheckBox chckbxCi_15;
+    private JCheckBox chckbxCi_16;
+    private JCheckBox chckbxCi_17;
+    private JCheckBox chckbxCi_18;
+    private JCheckBox chckbxCi_19;
+    private JCheckBox chckbxCi_20;
+    private JCheckBox chckbxCi_21;
+    private JCheckBox chckbxCi_22;
+    private JCheckBox chckbxCi_23;
+    private JCheckBox chckbxCi_24;
     private final JPanel panel = new JPanel();
-    private JTextField textField_3;
+    private ArrayList<String> materiasSolicitadas = new ArrayList<String>();
 
     /**
      * Launch the application.
@@ -62,32 +90,34 @@ public class Interface {
         frame = new JFrame();
         frame.getContentPane().setBackground(Color.WHITE);
         frame.setBackground(Color.WHITE);
-        frame.setBounds(100, 100, 1324, 1000);
+        frame.setBounds(100, 100, 1290, 1000);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(null);
 
         JLabel lblNome = new JLabel("Nome: ");
         lblNome.setFont(new Font("DejaVu Sans Condensed", Font.BOLD, 24));
         lblNome.setForeground(SystemColor.activeCaption);
-        lblNome.setBounds(50, 209, 77, 29);
+        lblNome.setBounds(50, 209, 100, 29);
         frame.getContentPane().add(lblNome);
 
-        textField = new JTextField();
-        textField.setBounds(150, 209, 626, 29);
-        frame.getContentPane().add(textField);
-        textField.setColumns(10);
+        textNome = new JTextField();
+        textNome.setBounds(155, 209, 626, 29);
+        frame.getContentPane().add(textNome);
+        textNome.setColumns(10);
 
         JLabel lblGrr = new JLabel("GRR:");
-        lblGrr.setBounds(795, 209, 58, 29);
+        lblGrr.setFont(new Font("DejaVu Sans Condensed", Font.BOLD, 24));
+        lblGrr.setBounds(795, 209, 70, 29);
         frame.getContentPane().add(lblGrr);
 
-        textField_1 = new JTextField();
-        textField_1.setBounds(865, 209, 397, 29);
-        frame.getContentPane().add(textField_1);
-        textField_1.setColumns(10);
+        textGrr = new JTextField();
+        textGrr.setBounds(865, 209, 397, 29);
+        frame.getContentPane().add(textGrr);
+        textGrr.setColumns(10);
 
         JLabel lblGradeQueEst = new JLabel("Grade que está matriculado:");
-        lblGradeQueEst.setBounds(56, 291, 305, 29);
+        lblGradeQueEst.setFont(new Font("DejaVu Sans Condensed", Font.BOLD, 24));
+        lblGradeQueEst.setBounds(56, 291, 318, 29);
         frame.getContentPane().add(lblGradeQueEst);
 
         JCheckBox checkBox = new JCheckBox("2011");
@@ -102,14 +132,15 @@ public class Interface {
         frame.getContentPane().add(checkBox_1);
 
         JLabel lblHistrico = new JLabel("Histórico:");
-        lblHistrico.setBounds(640, 291, 117, 29);
+        lblHistrico.setFont(new Font("DejaVu Sans Condensed", Font.BOLD, 24));
+        lblHistrico.setBounds(640, 291, 120, 29);
         frame.getContentPane().add(lblHistrico);
 
         // Mostra o caminho que foi guardado
-        textField_2 = new JTextField();
-        textField_2.setBounds(866, 288, 85, 35);
-        frame.getContentPane().add(textField_2);
-        textField_2.setColumns(10);
+        textCaminho = new JTextField();
+        textCaminho.setBounds(866, 288, 85, 35);
+        frame.getContentPane().add(textCaminho);
+        textCaminho.setColumns(10);
 
         JButton btnBuscar = new JButton("Buscar");
         btnBuscar.setBounds(765, 288, 100, 35);
@@ -136,138 +167,140 @@ public class Interface {
 
         JLabel lblQuaisDisciplinasVoc = new JLabel("Quais disciplinas você quer solicitar quebra?");
         lblQuaisDisciplinasVoc.setForeground(SystemColor.activeCaption);
+        lblQuaisDisciplinasVoc.setFont(new Font("DejaVu Sans Condensed", Font.BOLD, 24));
         lblQuaisDisciplinasVoc.setBounds(56, 366, 443, 29);
         frame.getContentPane().add(lblQuaisDisciplinasVoc);
 
-        JCheckBox chckbxCi_1 = new JCheckBox("CI1059");
+        chckbxCi_1 = new JCheckBox("CI1059");
         chckbxCi_1.setBackground(Color.WHITE);
         chckbxCi_1.setBounds(59, 419, 129, 23);
         frame.getContentPane().add(chckbxCi_1);
 
-        JCheckBox chckbxCi_2 = new JCheckBox("CI1350");
+        chckbxCi_2 = new JCheckBox("CI1350");
         chckbxCi_2.setBackground(Color.WHITE);
         chckbxCi_2.setBounds(59, 459, 129, 23);
         frame.getContentPane().add(chckbxCi_2);
 
-        JCheckBox chckbxCi = new JCheckBox("CI1165");
+        chckbxCi = new JCheckBox("CI1165");
         chckbxCi.setBackground(Color.WHITE);
         chckbxCi.setBounds(59, 499, 129, 23);
         frame.getContentPane().add(chckbxCi);
 
-        JCheckBox chckbxCi_3 = new JCheckBox("CI1164");
+        chckbxCi_3 = new JCheckBox("CI1164");
         chckbxCi_3.setBackground(Color.WHITE);
         chckbxCi_3.setBounds(59, 539, 129, 23);
         frame.getContentPane().add(chckbxCi_3);
 
-        JCheckBox chckbxCi_4 = new JCheckBox("CI1062");
+        chckbxCi_4 = new JCheckBox("CI1062");
         chckbxCi_4.setBackground(Color.WHITE);
         chckbxCi_4.setBounds(59, 579, 129, 23);
         frame.getContentPane().add(chckbxCi_4);
 
-        JCheckBox chckbxCi_5 = new JCheckBox("CI1065");
+        chckbxCi_5 = new JCheckBox("CI1065");
         chckbxCi_5.setBackground(Color.WHITE);
         chckbxCi_5.setBounds(199, 419, 129, 23);
         frame.getContentPane().add(chckbxCi_5);
 
-        JCheckBox chckbxCi_6 = new JCheckBox("CI1058");
+        chckbxCi_6 = new JCheckBox("CI1058");
         chckbxCi_6.setBackground(Color.WHITE);
         chckbxCi_6.setBounds(199, 459, 129, 23);
         frame.getContentPane().add(chckbxCi_6);
 
-        JCheckBox chckbxCi_7 = new JCheckBox("CI1238");
+        chckbxCi_7 = new JCheckBox("CI1238");
         chckbxCi_7.setBackground(Color.WHITE);
         chckbxCi_7.setBounds(199, 499, 129, 23);
         frame.getContentPane().add(chckbxCi_7);
 
-        JCheckBox chckbxCi_8 = new JCheckBox("CI1162");
+        chckbxCi_8 = new JCheckBox("CI1162");
         chckbxCi_8.setBackground(Color.WHITE);
         chckbxCi_8.setBounds(199, 539, 129, 23);
         frame.getContentPane().add(chckbxCi_8);
 
-        JCheckBox chckbxCi_9 = new JCheckBox("CI1215");
+        chckbxCi_9 = new JCheckBox("CI1215");
         chckbxCi_9.setBackground(Color.WHITE);
         chckbxCi_9.setBounds(199, 579, 129, 23);
         frame.getContentPane().add(chckbxCi_9);
 
-        JCheckBox chckbxCi_10 = new JCheckBox("CI1209");
+        chckbxCi_10 = new JCheckBox("CI1209");
         chckbxCi_10.setBackground(Color.WHITE);
         chckbxCi_10.setBounds(339, 419, 129, 23);
         frame.getContentPane().add(chckbxCi_10);
 
-        JCheckBox chckbxCi_11 = new JCheckBox("CI1218");
+        chckbxCi_11 = new JCheckBox("CI1218");
         chckbxCi_11.setBackground(Color.WHITE);
         chckbxCi_11.setBounds(339, 459, 129, 23);
         frame.getContentPane().add(chckbxCi_11);
 
-        JCheckBox chckbxCi_12 = new JCheckBox("CI1061");
+        chckbxCi_12 = new JCheckBox("CI1061");
         chckbxCi_12.setBackground(Color.WHITE);
         chckbxCi_12.setBounds(339, 499, 129, 23);
         frame.getContentPane().add(chckbxCi_12);
 
-        JCheckBox chckbxCi_13 = new JCheckBox("CI1316");
+        chckbxCi_13 = new JCheckBox("CI1316");
         chckbxCi_13.setBackground(Color.WHITE);
         chckbxCi_13.setBounds(339, 539, 129, 23);
         frame.getContentPane().add(chckbxCi_13);
 
-        JCheckBox chckbxCi_14 = new JCheckBox("CI1163");
+        chckbxCi_14 = new JCheckBox("CI1163");
         chckbxCi_14.setBackground(Color.WHITE);
         chckbxCi_14.setBounds(339, 579, 129, 23);
         frame.getContentPane().add(chckbxCi_14);
 
-        JCheckBox chckbxCi_15 = new JCheckBox("CI1221");
+        chckbxCi_15 = new JCheckBox("CI1221");
         chckbxCi_15.setBackground(Color.WHITE);
         chckbxCi_15.setBounds(479, 419, 129, 23);
         frame.getContentPane().add(chckbxCi_15);
 
-        JCheckBox chckbxCi_16 = new JCheckBox("CI1007");
+        chckbxCi_16 = new JCheckBox("CI1007");
         chckbxCi_16.setBackground(Color.WHITE);
         chckbxCi_16.setBounds(479, 459, 129, 23);
         frame.getContentPane().add(chckbxCi_16);
 
-        JCheckBox chckbxCi_17 = new JCheckBox("CI1211");
+        chckbxCi_17 = new JCheckBox("CI1211");
         chckbxCi_17.setBackground(Color.WHITE);
         chckbxCi_17.setBounds(479, 499, 129, 23);
         frame.getContentPane().add(chckbxCi_17);
 
-        JCheckBox chckbxCi_18 = new JCheckBox("CI1005");
+        chckbxCi_18 = new JCheckBox("CI1005");
         chckbxCi_18.setBackground(Color.WHITE);
         chckbxCi_18.setBounds(479, 539, 129, 23);
         frame.getContentPane().add(chckbxCi_18);
 
-        JCheckBox chckbxCi_19 = new JCheckBox("CI1064");
+        chckbxCi_19 = new JCheckBox("CI1064");
         chckbxCi_19.setBackground(Color.WHITE);
         chckbxCi_19.setBounds(479, 579, 129, 23);
         frame.getContentPane().add(chckbxCi_19);
 
-        JCheckBox chckbxCi_20 = new JCheckBox("CI1100");
+        chckbxCi_20 = new JCheckBox("CI1100");
         chckbxCi_20.setBackground(Color.WHITE);
         chckbxCi_20.setBounds(609, 419, 129, 23);
         frame.getContentPane().add(chckbxCi_20);
 
-        JCheckBox chckbxCi_21 = new JCheckBox("______");
+        chckbxCi_21 = new JCheckBox("______");
         chckbxCi_21.setBackground(Color.WHITE);
         chckbxCi_21.setBounds(609, 459, 129, 23);
         frame.getContentPane().add(chckbxCi_21);
 
-        JCheckBox chckbxCi_22 = new JCheckBox("______");
+        chckbxCi_22 = new JCheckBox("______");
         chckbxCi_22.setBackground(Color.WHITE);
         chckbxCi_22.setBounds(609, 499, 129, 23);
         frame.getContentPane().add(chckbxCi_22);
 
-        JCheckBox chckbxCi_23 = new JCheckBox("______");
+        chckbxCi_23 = new JCheckBox("______");
         chckbxCi_23.setBackground(Color.WHITE);
         chckbxCi_23.setBounds(609, 539, 129, 23);
         frame.getContentPane().add(chckbxCi_23);
 
-        JCheckBox chckbxCi_24 = new JCheckBox("______");
+        chckbxCi_24 = new JCheckBox("______");
         chckbxCi_24.setBackground(Color.WHITE);
         chckbxCi_24.setBounds(609, 579, 129, 23);
         frame.getContentPane().add(chckbxCi_24);
 
         JLabel lblSemConflito = new JLabel(
                 "Tem condições de se matricular nesta(s) disciplina(s), sem colisão de horário com disciplinas já matriculado?");
+        lblNome.setFont(new Font("DejaVu Sans Condensed", Font.BOLD, 24));
         lblSemConflito.setForeground(SystemColor.activeCaption);
-        lblSemConflito.setBounds(56, 666, 1229, 58);
+        lblSemConflito.setBounds(56, 666, 1124, 58);
         frame.getContentPane().add(lblSemConflito);
 
         JCheckBox checkBox_4 = new JCheckBox("Sim");
@@ -281,16 +314,24 @@ public class Interface {
         frame.getContentPane().add(checkBox_5);
 
         JLabel lblJustificativa = new JLabel("Justifique seus pedidos e informe a");
+        lblJustificativa.setFont(new Font("DejaVu Sans Condensed", Font.BOLD, 24));
         lblJustificativa.setForeground(SystemColor.activeCaption);
-        lblJustificativa.setBounds(807, 366, 421, 29);
+        lblJustificativa.setBounds(807, 366, 460, 29);
         frame.getContentPane().add(lblJustificativa);
 
         JLabel lblPrioridade = new JLabel("ordem de prioridade de cada uma:");
+        lblPrioridade.setFont(new Font("DejaVu Sans Condensed", Font.BOLD, 24));
         lblPrioridade.setForeground(SystemColor.activeCaption);
-        lblPrioridade.setBounds(809, 402, 407, 29);
+        lblPrioridade.setBounds(809, 402, 450, 29);
         frame.getContentPane().add(lblPrioridade);
 
         JButton btnEnviarPedido = new JButton("Enviar Pedido");
+        btnEnviarPedido.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                verificaMateriasSelecionadas();
+                JOptionPane.showMessageDialog(null, "Pedido enviado! FAZER OUTRA TELA ." + materiasSolicitadas);
+            }
+        });
         btnEnviarPedido.setBounds(507, 788, 282, 67);
         frame.getContentPane().add(btnEnviarPedido);
 
@@ -320,7 +361,54 @@ public class Interface {
         int retorno = choose.showSaveDialog(null);
         if (retorno == JFileChooser.APPROVE_OPTION) {
             caminhoArquivo = choose.getSelectedFile().getAbsolutePath();
-            textField_2.setText(caminhoArquivo);
+            this.textCaminho.setText(caminhoArquivo);
         }
+    }
+
+    public void verificaMateriasSelecionadas() {
+        if (this.chckbxCi_1.isSelected())
+            this.materiasSolicitadas.add("CI1059");
+        if (this.chckbxCi_2.isSelected())
+            this.materiasSolicitadas.add("CI1350");
+        if (this.chckbxCi.isSelected())
+            this.materiasSolicitadas.add("CI1165");
+        if (this.chckbxCi_3.isSelected())
+            this.materiasSolicitadas.add("CI1164");
+        if (this.chckbxCi_4.isSelected())
+            this.materiasSolicitadas.add("CI1062");
+        if (this.chckbxCi_5.isSelected())
+            this.materiasSolicitadas.add("CI1065");
+        if (this.chckbxCi_6.isSelected())
+            this.materiasSolicitadas.add("CI1058");
+        if (this.chckbxCi_7.isSelected())
+            this.materiasSolicitadas.add("CI1238");
+        if (this.chckbxCi_8.isSelected())
+            this.materiasSolicitadas.add("CI1162");
+        if (this.chckbxCi_9.isSelected())
+            this.materiasSolicitadas.add("CI1215");
+        if (this.chckbxCi_10.isSelected())
+            this.materiasSolicitadas.add("CI1209");
+        if (this.chckbxCi_11.isSelected())
+            this.materiasSolicitadas.add("CI1218");
+        if (this.chckbxCi_12.isSelected())
+            this.materiasSolicitadas.add("CI1061");
+        if (this.chckbxCi_13.isSelected())
+            this.materiasSolicitadas.add("CI1316");
+        if (this.chckbxCi_14.isSelected())
+            this.materiasSolicitadas.add("CI1163");
+        if (this.chckbxCi_15.isSelected())
+            this.materiasSolicitadas.add("CI1221");
+        if (this.chckbxCi_16.isSelected())
+            this.materiasSolicitadas.add("CI1007");
+        if (this.chckbxCi_17.isSelected())
+            this.materiasSolicitadas.add("CI1211");
+        if (this.chckbxCi_18.isSelected())
+            this.materiasSolicitadas.add("CI1005");
+        if (this.chckbxCi_19.isSelected())
+            this.materiasSolicitadas.add("CI1064");
+        if (this.chckbxCi_20.isSelected())
+            this.materiasSolicitadas.add("CI1100");
+        if (this.chckbxCi_21.isSelected())
+            this.materiasSolicitadas.add("______");
     }
 }
