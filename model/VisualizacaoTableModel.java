1673ee32d6ad3abaf0a453f8fc2ac7646cca1631
leticiafontanelli <leticiafontanelli.ll@gmail.com>
2022-05-01 17:25:33 -0300

    Adiciona manualmente tabela

--- model/VisualizacaoTableModel.java
+++ model/VisualizacaoTableModel.java
@@ -10,25 +10,28 @@ import javax.swing.JTextField;
 import javax.swing.table.AbstractTableModel;
 
 public class VisualizacaoTableModel extends AbstractTableModel {
-    private String[] colunas = new String[] { "1", "2", "3", "4", "5", "6", "7", "8" };
-    private String[] lin1 = new String[] { "CI1068", "CI1210", "CI1212", "CI1059", "CI1065", "CI1209", "CI1221",
-            "CI1211" };
-    private String[] lin2 = new String[] { "CI1055", "CI1056", "CI1057", "CI1350", "CI1058", "CI1218", "CI1007",
-            "CI1005" };
-    private String[] lin3 = new String[] { "CI1003", "CI1001", "CI1002", "CI1165", "CI1238", "CI1061", "OPT", "OPT" };
-    private String[] lin4 = new String[] { "CMA111", "CMA211", "CI1237", "CI1164", "CI1162", "CI1316", "OPT", "OPT" };
-    private String[] lin5 = new String[] { "CM304", "CM303", "CE009", "CI1062", "CI1215", "CI1163", "TCC I", "TCC II" };
-    private String[] lin6 = new String[] { "", "", "", "CI1064", "OPT", "OPT", "OPT", "CI1100", "" };
-    private List<String[]> codigos = new ArrayList<String[]>();
+    private String[] colunas = { "1", "2", "3" };
+    // private String [] lin1 = new String [] {"CI1068","CI1210","CI1212"};
+    // private String [] lin2 = new String [] {"CI1055","CI1056","CI1057"};
+    // private String [] lin3 = new String [] {"CI1003","CI1001", "CI1002"};
+    // private String [] lin4 = new String [] {"CMA111", "CMA211", "CI1237"};
+    // private String [] lin5 = new String [] {"CM304", "CM303", "CE009"};
+    // private List<String[]> codigos = new ArrayList<String[]>();
+    private Object[][] linhas = {
+            { "CI1068", "CI1210", "CI1212" },
+            { "CI1055", "CI1056", "CI1057" },
+            { "CI1003", "CI1001", "CI1002" },
+            { "CMA111", "CMA211", "CI1237" },
+            { "CM304", "CM303", "CE009" },
+    };
 
-    private void adicionacodigos() {
-        this.codigos.add(lin1);
-        this.codigos.add(lin2);
-        this.codigos.add(lin3);
-        this.codigos.add(lin4);
-        this.codigos.add(lin5);
-        this.codigos.add(lin6);
-    }
+    // private void adicionacodigos() {
+    // this.codigos.add(lin1);
+    // this.codigos.add(lin2);
+    // this.codigos.add(lin3);
+    // this.codigos.add(lin4);
+    // this.codigos.add(lin5);
+    // }
 
     @Override
     public int getColumnCount() {
@@ -37,32 +40,19 @@ public class VisualizacaoTableModel extends AbstractTableModel {
 
     @Override
     public int getRowCount() {
-        return this.codigos.size();
+        return this.linhas.length;
     }
 
     @Override
-	public Object getValueAt(int linha, int coluna) {
-		adicionacodigos();
-		switch(coluna) {
-		case 0:
-			return this.codigos.get(linha).get???;
-		case 1:
-			return this.codigos.get(linha);
-		}
-		case 2:
-			return this.codigos.get(linha).get???;
-		case 3:
-			return this.codigos.get(linha).get???;
-		case 4:
-			return this.codigos.get(linha).get???;
-		case 5:
-			return this.codigos.get(linha).get???;
-		case 6:
-			return this.codigos.get(linha).get???;
-		case 7:
-			return this.codigos.get(linha).get???;
-		case 8:
-			return this.codigos.get(linha).get???;		
-		
-	}
+    public Object getValueAt(int linha, int coluna) {
+        switch (coluna) {
+            case 0:
+                return linhas.get(linha).get(coluna);
+            case 1:
+                return "b";
+            case 2:
+                return "c";
+        }
+        return null;
+    }
 }
--- view/VisualizacaoUsuario.java
+++ view/VisualizacaoUsuario.java
@@ -22,6 +22,7 @@ import javax.swing.table.DefaultTableModel;
 
 import model.DadosRecebidosFormulario;
 import model.VisualizacaoTableModel;
+import javax.swing.border.LineBorder;
 
 public class VisualizacaoUsuario extends JFrame {
 
@@ -36,7 +37,7 @@ public class VisualizacaoUsuario extends JFrame {
 	private JTable tableCodigos;
 	private DadosRecebidosFormulario dados = new DadosRecebidosFormulario();
 	VisualizacaoTableModel table = new VisualizacaoTableModel();
-	
+
 	/**
 	 * Launch the application.
 	 */
@@ -57,23 +58,23 @@ public class VisualizacaoUsuario extends JFrame {
 	 * Create the frame.
 	 */
 	public VisualizacaoUsuario() {
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 918, 525);
+		setBounds(100, 100, 918, 446);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBorder(null);
 		panel.setBackground(Color.WHITE);
-		panel.setBounds(12, 0, 906, 496);
+		panel.setBounds(12, 0, 906, 407);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
-//		textNome = new JTextField(this.dados.getNome());
+
+		// textNome = new JTextField(this.dados.getNome());
 		textNome = new JTextField();
 		textNome.setFont(new Font("Dialog", Font.PLAIN, 12));
 		textNome.setText("NOME");
@@ -83,13 +84,13 @@ public class VisualizacaoUsuario extends JFrame {
 		textNome.setBorder(null);
 		panel.add(textNome);
 		textNome.setColumns(10);
-		
+
 		JLabel lblMatrcula = new JLabel("Matrícula:");
 		lblMatrcula.setFont(new Font("Dialog", Font.BOLD, 15));
 		lblMatrcula.setBounds(228, 35, 97, 15);
 		panel.add(lblMatrcula);
-		
-//		textGrr = new JTextField(this.dados.getGrr());
+
+		// textGrr = new JTextField(this.dados.getGrr());
 		textGrr = new JTextField();
 		textGrr.setFont(new Font("Dialog", Font.PLAIN, 15));
 		textGrr.setText("GRR");
@@ -99,13 +100,13 @@ public class VisualizacaoUsuario extends JFrame {
 		textGrr.setBorder(null);
 		panel.add(textGrr);
 		textGrr.setColumns(10);
-		
+
 		lblPerodoAtual = new JLabel("Período Atual:");
 		lblPerodoAtual.setFont(new Font("Dialog", Font.BOLD, 15));
 		lblPerodoAtual.setBounds(448, 35, 119, 15);
 		panel.add(lblPerodoAtual);
-		
-//		textPeriodo = new JTextField(this.dados.getPeriodoAtual());
+
+		// textPeriodo = new JTextField(this.dados.getPeriodoAtual());
 		textPeriodo = new JTextField();
 		textPeriodo.setFont(new Font("Dialog", Font.PLAIN, 15));
 		textPeriodo.setText("P");
@@ -115,13 +116,13 @@ public class VisualizacaoUsuario extends JFrame {
 		textPeriodo.setBorder(null);
 		panel.add(textPeriodo);
 		textPeriodo.setColumns(10);
-		
+
 		lblIra = new JLabel("IRA:");
 		lblIra.setFont(new Font("Dialog", Font.BOLD, 15));
 		lblIra.setBounds(612, 35, 70, 15);
 		panel.add(lblIra);
-		
-//		textIra = new JTextField(this.alunoCadastrado.getIra());
+
+		// textIra = new JTextField(this.alunoCadastrado.getIra());
 		textIra = new JTextField();
 		textIra.setFont(new Font("Dialog", Font.PLAIN, 15));
 		textIra.setText("IRA");
@@ -131,29 +132,51 @@ public class VisualizacaoUsuario extends JFrame {
 		textIra.setBorder(null);
 		panel.add(textIra);
 		textIra.setColumns(10);
-		
+
 		JButton btnEditar = new JButton("Editar Pedido");
 		btnEditar.setFont(new Font("Dialog", Font.BOLD, 12));
 		btnEditar.setBackground(new Color(51, 204, 204));
 		btnEditar.setBounds(745, 78, 143, 62);
 		panel.add(btnEditar);
-		
+
 		JButton btnVer = new JButton("Ver Pedido");
 		btnVer.setBackground(new Color(51, 204, 204));
 		btnVer.setBounds(745, 158, 143, 62);
 		panel.add(btnVer);
-		
+
 		textSituacao = new JTextArea();
+		textSituacao.setForeground(Color.BLACK);
+		textSituacao.setFont(new Font("Dialog", Font.PLAIN, 18));
+		textSituacao.setText("Você está no caso");
 		textSituacao.setBackground(new Color(255, 153, 51));
 		textSituacao.setEditable(false);
 		textSituacao.setBounds(745, 248, 143, 138);
 		panel.add(textSituacao);
-		
+
 		tableCodigos = new JTable();
-		tableCodigos.setBounds(40, 78, 642, 310);
-		tableCodigos.setModel(table);
+		tableCodigos.setBorder(new LineBorder(new Color(0, 0, 0)));
+		tableCodigos.setBounds(40, 102, 519, 96);
+		tableCodigos.setModel(new DefaultTableModel(
+				new Object[][] {
+						{ "1", "2", "3" },
+						{ "CI1068", "CI1210", "CI1212" },
+						{ "CI1055", "CI1056", "CI1057" },
+						{ "CI1003", "CI1001", "CI1002" },
+						{ "CMA111", "CMA211", "CI1237" },
+						{ "CM304", "CM303", "CE009" },
+				},
+				new String[] {
+						"1", "2", "3"
+				}) {
+			Class[] columnTypes = new Class[] {
+					String.class, String.class, String.class
+			};
+
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
 		panel.add(tableCodigos);
 
-		
 	}
 }
